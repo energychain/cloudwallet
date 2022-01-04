d838e251a8821b3551ae5b10f25b196d232a1cda
Thorsten Zoerner <me@thorsten-zoerner.com>
2022-01-04 14:42:25 +0100

    Initial Implementation

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # cloudwallet
-Cloudwallet Implementation
+
+<a href="https://stromdao.de/" target="_blank" title="STROMDAO - Digital Energy Infrastructure"><img src="./static/stromdao.png" align="right" height="85px" hspace="30px" vspace="30px"></a>
+
+
+**Cloudwallet Implementation**
+
+[![npm](https://img.shields.io/npm/dt/cloudwallet.svg)](https://www.npmjs.com/package/cloudwallet)
+[![npm](https://img.shields.io/npm/v/cloudwallet.svg)](https://www.npmjs.com/package/cloudwallet)
+[![CO2Offset](https://api.corrently.io/v2.0/ghgmanage/statusimg?host=cloudwallet&svg=1)](https://co2offset.io/badge.html?host=cloudwallet)[![Code Quality](https://api.codiga.io/project/30556/score/svg)](https://app.codiga.io/public/project/30556/cloudwallet/dashboard)[![CircleCI](https://circleci.com/gh/energychain/cloudwallet/tree/main.svg?style=svg)](https://circleci.com/gh/energychain/cloudwallet/tree/main
+
+## API Usage
+
+You need to register for the [Cloudwallet-API on RapidAPI](https://rapidapi.com/stromdao-stromdao-default/api/cloudwallet). Replace `rapidApiKey` with yours.
+
+```javascript
+
+const Cloudwallet = require("cloudwallet");
+const instance1 = new Cloudwallet('<rapidApiKey>'); // Create a new KeyPair
+await instance1.set('Hello','World'); // Store a Keyvalue like a presentation
+
+// ReUse wallet remembering privateKey and identifier
+const keys = instance1.getKeypair();
+console.log(keys);
+
+const instance2 = new Cloudwallet('<rapidAPIkey',keys.privateKey,keys.identifier);
+console.log(await instance2.get('Hello'));
+```
---
+++ lib.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const axios = require("axios");
+const EthrDID = require("ethr-did").EthrDID;
+
+const cloudwallet = function(rapidAPIkey,keypair,identifier) {
+
+  let baseURL = 'https://cloudwallet.p.rapidapi.com/';
+
+  let parent = this;
+
+  if((typeof keypair == 'undefined')||(keypair == null)) {
+    keypair = EthrDID.createKeyPair();
+    keypair.id = "did:ethr:"+keypair.identifier;
+  }
+  if(typeof keypair == 'string') {
+    keypair = {
+      privateKey:keypair,
+      identifier:identifier
+    }
+  }
+  const headers = {
+        "content-type":"application/json",
+        "x-rapidapi-host":"cloudwallet.p.rapidapi.com",
+        "x-rapidapi-key":rapidAPIkey,
+        "useQueryString":true
+  };
+
+  const _call = async function() {
+    const ethrDid = new EthrDID({
+      identifier:keypair.identifier,
+      rpcUrl:"https://integration.corrently.io/",
+      name: "mainnet",
+      chainId: "6226",
+      registry:"0xda77BEeb5002e10be2F5B63E81Ce8cA8286D4335",
+      privateKey:keypair.privateKey
+     });
+    const jwt = await ethrDid.signJWT(parent);
+    const settings = {
+          "method":"POST",
+          "url":baseURL+"cloudwallet",
+          "headers":headers,
+          "params":{
+            "did":jwt
+          }
+    };
+    const responds = await axios(settings);
+    for (const [key, value] of Object.entries(responds.data)) {
+      parent[key] = value;
+    }
+    return responds.data;
+  }
+
+  this.get = async function(key) {
+      await _call();
+      return parent[key];
+  }
+
+  this.set = async function(key,value) {
+      parent[key] = value;
+      await _call();
+  }
+
+  this.getKeypair = function() {
+    return keypair;
+  }
+
+  _call();
+}
+module.exports = cloudwallet;
